219c48cd5f1d57111b2c7c293759304fa6367571
SINGA SANJEEVARAJU <sanjeevaraj123@gmail.com>
2024-11-18 21:56:15 +0530

    files

---
+++ day16.js
@@ -0,0 +1,71 @@
+let date=new Date();
+console.log(date.getFullYear());
+let date=new Date();
+console.log(date.getMonth());
+let date=new Date();
+console.log(date.getDate());
+let date=new Date();
+console.log(date.getDay());
+let date=new Date();
+console.log(date.getHours());
+let date=new Date();
+console.log(date.getMinutes());
+let date=new Date();
+console.log(date.getSeconds());
+let date=new Date();
+console.log(date.getMilliseconds());
+let date=new Date();
+console.log(date.setFullYear(25));
+console.log(date.setMonth(12));
+console.log(date.setDate(25));
+console.log(date.setHours(24));
+console.log(date.setMinutes(30));
+console.log(date.setSeconds(15));
+console.log(date.setMilliseconds(55));
+console.log(date.toDateString());
+console.log(date.toTimeString());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleTimeString());
+
+
+let a=1.6;
+console.log(Math.round(a));
+let a=1.5;
+console.log(Math.round(a));
+let a=1.4;
+console.log(Math.round(a));
+
+let a=12.88;
+console.log(Math.ceil(a));
+let a=0.88;
+console.log(Math.ceil(a));
+let a=123.234;
+console.log(Math.floor(a));
+let a=-123.234;
+console.log(Math.floor(a));
+let a=144;
+console.log(Math.sqrt(a));
+let a=5
+console.log(Math.pow(a,3));
+let a=5
+console.log(Math.pow(a,3));
+let a=-5
+console.log(Math.abs(a));
+
+console.log(Math.min(0,-0,4,5,67));
+console.log(Math.max(0,-0,4,5,67));
+console.log(Math.floor(Math.random() *10));
+let a=9.0;
+console.log(Math.trunc(a));
+
+function a(){
+    console.log("surya");
+}a()
+
+const a=()=>console.log("vamsi")
+a()
+let b="surya"
+function a(){
+    console.log(`hi ${b} how are u`);
+}a()
+
